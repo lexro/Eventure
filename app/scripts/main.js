434b3434a416d38aac9da356c3f8242c7c00bd64
Lex Lacson <lex.lacson.dev@gmail.com>
2016-04-09 10:49:21 -0700

    navigation with history api, router, controller

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,43 @@
 console.log('------------ Event Planner -------------');
+
+(function () {
+  function EventPlannerApp () {
+    this.controller = new app.Controller();
+  }
+
+  var eventPlannerApp = new EventPlannerApp();
+
+  var routes = {
+    'home': function () {
+      console.log('go home');
+    },
+    'create': function () {
+      console.log('create event');
+    },
+    'signup': function () {
+      eventPlannerApp.controller.render('account-creation');
+    }
+  };
+
+  $(document).on('click', 'a', function (event){
+    var $this = $(this);
+    var path = $this.attr('data-name');
+
+    var route = routes[path];
+
+    if (route) {
+      history.pushState(null, null, '/' + path);
+      route();
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+  });
+
+  $(window).on('popstate', function (e) {
+      var state = e.originalEvent.state;
+      if (state !== null) {
+
+      }
+  });
+})();
