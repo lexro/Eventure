bb8899cf345da32116a60cc3c8c56a518d3f099c
Lex Lacson <lex.lacson.dev@gmail.com>
2016-05-01 22:57:59 -0700

    refactor account creation validation stuff

--- app/scripts/accounts/account-form-validator.js
+++ app/scripts/accounts/account-form-validator.js
@@ -1,15 +1,8 @@
+/**
+ * AccountFormValidator class that holds the logic for account creation validation
+ */
 function AccountFormValidator () {
-  var SELECTORS = {
-    firstPasswordInput: '#account-form__password',
-    confirmPasswordInput: '#account-form__confirm-password',
-    submitButton: '.account-form__submit-button'
-  };
-
-  var firstPasswordInput = document.querySelector(SELECTORS.firstPasswordInput);
-  var confirmPasswordInput = document.querySelector(SELECTORS.confirmPasswordInput);
-  var submitButton = document.querySelector(SELECTORS.submitButton);
-
-  var VALIDATIONS = {
+  this.validations = {
     sybmol: [/[\!\@\#\$\%\^\&\*]/g, 'Should have a symbol (!, @, #, $, %, ^, &, *)'],
     number: [/\d/g, 'Should have a number'],
     lowercase: [/[a-z]/g, 'Should have a lowercase letter'],
@@ -17,48 +10,56 @@ function AccountFormValidator () {
     illegalCharacter: [/[^A-z0-9\!\@\#\$\%\^\&\*]/g, 'Illegal Character']
   };
 
-  /**
-   * Checks a password against certain criteria including character length and symbols.
-   *
-   * @param  {String} password - The password to validate
-   * @return {Array<String>} - List of validation messages that the password needs to meet
-   */
-  function validatePassword(password) {
-    var minPasswordLength = 8;
-    var validationMessages = [];
-
-    if (password.length < minPasswordLength) {
-        validationMessages.push('Should be at least ' + minPasswordLength + ' characters');
-    }
-
-    for(var issue in VALIDATIONS) {
-      var validation = VALIDATIONS[issue];
-      if (!password.match(validation[0])) {
-        validationMessages.push(validation[1]);
-      }
-    }
-
-    return validationMessages;
-  }
-
-  submitButton.addEventListener('click', function () {
-    var firstPasswordValue = firstPasswordInput.value;
-    var confirmPasswordValue = confirmPasswordInput.value;
-    var validationMessages = [];
-    var validationMessage;
-
-    if (firstPasswordValue && firstPasswordValue !== confirmPasswordValue) {
-      validationMessages.push('Passwords should match');
-    }  else {
-      validationMessages = validatePassword(firstPasswordValue);
-    }
-
-    if (validationMessages.length) {
-      validationMessage = 'Please fix these issues:\n' + validationMessages.join('\n');
-      firstPasswordInput.setCustomValidity(validationMessage);
-    }
-  });
-
+  this.minPasswordLength = 8;
 }
 
-export default new AccountFormValidator();
+AccountFormValidator.prototype.validatePassword = function (firstPasswordValue) {
+  var validationMessages = [];
+  var validationMessage = '';
+
+  validationMessages = this._validatePassword(firstPasswordValue);
+
+  if (validationMessages.length) {
+    validationMessage = 'Please fix these issues:\n' + validationMessages.join('\n');
+  }
+
+  return validationMessage;
+};
+
+/**
+ * Checks a password against certain criteria including character length and symbols.
+ *
+ * @param  {String} password - The password to validate
+ * @return {Array<String>} - List of validation messages that the password needs to meet
+ */
+AccountFormValidator.prototype._validatePassword = function (password) {
+  var minPasswordLength = this.minPasswordLength;
+  var validationMessages = [];
+
+  if (password.length < minPasswordLength) {
+      validationMessages.push('Should be at least ' + minPasswordLength + ' characters');
+  }
+
+  for(var issue in this.validations) {
+    var validation = this.validations[issue];
+    if (!password.match(validation[0])) {
+      validationMessages.push(validation[1]);
+    }
+  }
+
+  return validationMessages;
+};
+
+/**
+ * Checks for equality between the two passwords
+ * @param  {String} firstPasswordValue   The original password
+ * @param  {String} confirmPasswordValue the retyped password
+ * @return {String}                      A validation message, null if nothing wrong
+ */
+AccountFormValidator.prototype.validateConfirmPassword = function (firstPasswordValue, confirmPasswordValue) {
+  if (firstPasswordValue && firstPasswordValue !== confirmPasswordValue) {
+    return  'Passwords should match';
+  }
+};
+
+export default AccountFormValidator;
--- app/scripts/accounts/account-form.js
+++ app/scripts/accounts/account-form.js
@@ -6,8 +6,65 @@ import AccountFormValidator from './account-form-validator';
  */
 function AccountForm() {
   this.selector = '.account-form';
+  this.selectors = {
+    firstPasswordInput: '#account-form__password',
+    confirmPasswordInput: '#account-form__confirm-password',
+    submitButton: '.account-form__submit-button'
+  };
   var formElement = document.querySelector(this.selector);
   this.formValidation = new FormValidation(formElement);
+  this.accountFormValidator = new AccountFormValidator();
+  this._setupValidation();
 }
+
+/**
+ * Adds listeners on form elements for validation
+ * @return {[type]} [description]
+ */
+AccountForm.prototype._setupValidation = function () {
+  var submitButton = document.querySelector(this.selectors.submitButton);
+  submitButton.addEventListener('click', this._validateAccount.bind(this));
+
+  var firstPasswordInput = document.querySelector(this.selectors.firstPasswordInput);
+  firstPasswordInput.addEventListener('change', function () {
+    this._validatePassword();
+  }.bind(this));
+
+  var confirmPasswordInput = document.querySelector(this.selectors.confirmPasswordInput);
+  confirmPasswordInput.addEventListener('change', function () {
+    this._validateConfirmPassword();
+  }.bind(this));
+};
+
+/**
+ * Validates all fields for account creation
+ */
+AccountForm.prototype._validateAccount = function () {
+  this._validatePassword();
+  this._validateConfirmPassword();
+};
+
+/**
+ * Validates the password field and sets the validation message
+ */
+AccountForm.prototype._validatePassword = function () {
+  var firstPasswordInput = document.querySelector(this.selectors.firstPasswordInput);
+  var firstPasswordValue = firstPasswordInput.value;
+  var validationMessage = this.accountFormValidator.validatePassword(firstPasswordValue);
+
+  firstPasswordInput.setCustomValidity(validationMessage);
+};
+
+/**
+ * Validates the confirm password field and sets the validation message
+ */
+AccountForm.prototype._validateConfirmPassword = function () {
+  var firstPasswordValue = document.querySelector(this.selectors.firstPasswordInput).value;
+  var confirmPasswordInput = document.querySelector(this.selectors.confirmPasswordInput);
+  var confirmPasswordValue = confirmPasswordInput.value;
+  var validationMessage = this.accountFormValidator.validateConfirmPassword(firstPasswordValue, confirmPasswordValue);
+
+  confirmPasswordInput.setCustomValidity(validationMessage);
+};
 
 export default AccountForm;
